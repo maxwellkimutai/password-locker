f5b05870f451b227c3f6189f0815e4e4f531eb77
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-09-29 23:25:34 +0300

    create credential_test file

---
+++ credentials_test.py
@@ -0,0 +1,10 @@
+import unittest # Importing the unittest module
+from credentials import Credentials
+
+class TestCredentials(unittest.TestCase):
+    '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
