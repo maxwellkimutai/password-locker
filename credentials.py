aae9ac3b9d9466c046c7158934403cc28ab4d2fa
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-09-29 23:27:02 +0300

    create credentials class

---
+++ credentials.py
@@ -0,0 +1,10 @@
+import random
+class Credentials:
+    '''
+    Test class that defines test cases for the credentials class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
+    
